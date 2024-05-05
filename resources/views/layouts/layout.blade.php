da497356c86da379e7fe0e6b71d2c87035a72818
berrybeaver <andreaswelly.2002@gmail.com>
2024-05-06 00:13:05 +0200

    M2 finished
    - aufgabe 3 n 4

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -3,6 +3,7 @@
    <head>
        <meta charset="UTF-8">
        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+           <meta name="csrf-token" content="{{ csrf_token() }}">
        <title>@yield('title')</title>
        <script src="{{ asset('js/cookiecheck.js') }}"></script>
        <!--<link href="{{ asset('css/default.css') }}" rel="stylesheet"> -->
